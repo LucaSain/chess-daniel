e566a111a68ae6bc36bd09ccb4ca9403245ee9f3
Malanca Daniel <malanca.daniel23@gmail.com>
2024-05-25 19:50:20 +0300

    added tests for verifying if number of found moves is correct

--- src/performance_test.rs
+++ src/performance_test.rs
@@ -52,3 +52,93 @@ fn perft5_startpos() {
     let mut game = ChessGame::new();
     assert_eq!(perft(&mut game, 5), 4865609);
 }
+
+#[test]
+fn perft1_kiwipete() {
+    let mut game =
+        ChessGame::from_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -")
+            .unwrap();
+    assert_eq!(perft(&mut game, 1), 48);
+}
+
+#[test]
+fn perft2_kiwipete() {
+    let mut game =
+        ChessGame::from_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -")
+            .unwrap();
+    assert_eq!(perft(&mut game, 2), 2039);
+}
+
+#[test]
+fn perft3_kiwipete() {
+    let mut game =
+        ChessGame::from_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -")
+            .unwrap();
+    assert_eq!(perft(&mut game, 3), 97862);
+}
+
+#[test]
+fn perft4_kiwipete() {
+    let mut game =
+        ChessGame::from_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -")
+            .unwrap();
+    assert_eq!(perft(&mut game, 4), 4085603);
+}
+
+#[test]
+fn perft1_position_3() {
+    let mut game = ChessGame::from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ").unwrap();
+    assert_eq!(perft(&mut game, 1), 14);
+}
+
+#[test]
+fn perft2_position_3() {
+    let mut game = ChessGame::from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ").unwrap();
+    assert_eq!(perft(&mut game, 2), 191);
+}
+
+#[test]
+fn perft3_position_3() {
+    let mut game = ChessGame::from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ").unwrap();
+    assert_eq!(perft(&mut game, 3), 2812);
+}
+
+#[test]
+fn perft4_position_3() {
+    let mut game = ChessGame::from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ").unwrap();
+    assert_eq!(perft(&mut game, 4), 43238);
+}
+
+#[test]
+fn perft5_position_3() {
+    let mut game = ChessGame::from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ").unwrap();
+    assert_eq!(perft(&mut game, 5), 674624);
+}
+
+#[test]
+fn perft1_position_5() {
+    let mut game =
+        ChessGame::from_fen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8").unwrap();
+    assert_eq!(perft(&mut game, 1), 44);
+}
+
+#[test]
+fn perft2_position_5() {
+    let mut game =
+        ChessGame::from_fen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8").unwrap();
+    assert_eq!(perft(&mut game, 2), 1486);
+}
+
+#[test]
+fn perft3_position_5() {
+    let mut game =
+        ChessGame::from_fen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8").unwrap();
+    assert_eq!(perft(&mut game, 3), 62379);
+}
+
+#[test]
+fn perft4_position_5() {
+    let mut game =
+        ChessGame::from_fen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8").unwrap();
+    assert_eq!(perft(&mut game, 4), 2103487);
+}
